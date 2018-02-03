7debb95ecb5ef625959b92b7617144dc42564ff9
Svility <36098020+Svility@users.noreply.github.com>
2018-02-03 10:33:06 +0300

    Create qchainage_ru_RU.ts

---
+++ i18n/qchainage_ru_RU.ts
@@ -0,0 +1,124 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="2.0" language="ru_RU" sourcelanguage="en_GB">
+<context>
+    <name>QChainageDialog</name>
+    <message>
+        <location filename="ui_qchainage.py" line="166"/>
+        <source>Dialog</source>
+        <translation type="obsolete">Dialog</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="194"/>
+        <source>Select Layer to chainage</source>
+        <translation>Выберите слой</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="193"/>
+        <source>Chainage every</source>
+        <translation>Заменить все</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="203"/>
+        <source>Units</source>
+        <translation>Еденица</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="188"/>
+        <source>Force last point on feature</source>
+        <translation type="obsolete">Создать объект на крайней вершине?</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="192"/>
+        <source>Output Layername</source>
+        <translation>Название слоя с метками</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="208"/>
+        <source>Automatically Label the Layer</source>
+        <translation>Автоматически помечать слои</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="197"/>
+        <source>Basic</source>
+        <translation>Общие</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="198"/>
+        <source>End at</source>
+        <translation>Закончить на</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="206"/>
+        <source>all Features in Layer</source>
+        <translation>все объекты в слое</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="200"/>
+        <source>only selected Features</source>
+        <translation>только выбранные элементы</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="201"/>
+        <source>Start from</source>
+        <translation>Начать с</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="202"/>
+        <source>Chainage</source>
+        <translation>Километраж</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="207"/>
+        <source>Advanced</source>
+        <translation>Расширенный</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="190"/>
+        <source>QChainage</source>
+        <translation>QChainage</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="204"/>
+        <source>Divide Feature into</source>
+        <translation>Разделить объект на</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="205"/>
+        <source>Parts</source>
+        <translation>Части</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="209"/>
+        <source>Decimal places</source>
+        <translation>Десятичный знак</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="210"/>
+        <source>Labeling</source>
+        <translation>Надпись</translation>
+    </message>
+    <message>
+        <location filename="qchainage.py" line="103"/>
+        <source>No layers with line features - chainage not useful!</source>
+        <translation type="obsolete">Линейный слой не выбран!</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="195"/>
+        <source>Only first and last point</source>
+        <translation>Только первая и последняя точки</translation>
+    </message>
+    <message>
+        <location filename="ui_qchainage.py" line="196"/>
+        <source>Force last point</source>
+        <translation>Создать точку в конце?</translation>
+    </message>
+</context>
+<context>
+    <name>qchainage</name>
+    <message>
+        <location filename="qchainage.py" line="101"/>
+        <source>No layers with line features - chainage not useful!</source>
+        <translation type="obsolete">Линейный слой не выбран!</translation>
+    </message>
+</context>
+</TS>
